fb479fa1d9a3a29b5a7462089639d380e6d35fd3
Kelvin Hoover <kelvin.hoover@hotmail.com>
2018-04-11 23:00:39 -0700

    License headers

---
+++ Src/Dev/Khooversoft.AspMvc/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) KhooverSoft. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Khooversoft.Net;
+using Khooversoft.Toolbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Khooversoft.AspMvc
+{
+    /// <summary>
+    /// Exception extensions
+    /// </summary>
+    public static class ExceptionExtensions
+    {
+        /// <summary>
+        /// Convert known exception to HTTP status code
+        /// </summary>
+        /// <param name="exception">exception</param>
+        /// <returns>http status code, or internal server error as default</returns>
+        public static HttpStatusCode ToHttpStatusCode(this Exception exception)
+        {
+            // if it's not one of the expected exception, set it to 500
+            var code = HttpStatusCode.InternalServerError;
+
+            switch (exception)
+            {
+                case NotAuthorizedException x:
+                    code = HttpStatusCode.Unauthorized;
+                    break;
+
+                case ETagException x:
+                    code = HttpStatusCode.NotModified;
+                    break;
+
+                case NotFoundException x:
+                    code = HttpStatusCode.NotFound;
+                    break;
+
+                case BadRequestException x:
+                    code = HttpStatusCode.BadRequest;
+                    break;
+
+                case ArgumentNullException x:
+                    code = HttpStatusCode.BadRequest;
+                    break;
+
+                case ArgumentOutOfRangeException x:
+                    code = HttpStatusCode.BadRequest;
+                    break;
+
+                case ArgumentException x:
+                    code = HttpStatusCode.BadRequest;
+                    break;
+
+                default:
+                    code = HttpStatusCode.InternalServerError;
+                    break;
+            }
+
+            return code;
+        }
+    }
+}
